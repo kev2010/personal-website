9f3eaa30a8cd95c2f28a4e40efb6e41830d9ccf7
Kevin Jiang <kevin@popmeals.com>
2021-06-04 18:03:47 -0400

    Create Position.vue

---
+++ src/components/Position.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="position">
+        <div class="main">
+            <div>{{ title }}</div>
+            <div>{{ date }}</div>
+        </div>
+        <p class="description">{{ description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Position",
+    props: ['title', 'date', 'description']
+};
+</script>
+
+<style scoped>
+    .position {
+        display: flex;
+        flex-direction: column;
+        color: white;
+        background: var(--primary-light);
+    }
+
+    .main {
+        display: flex;
+        justify-content: space-between;
+        font-weight: 600;
+        font-size: 18px;
+    }
+
+    .description {
+        font-weight: 300; 
+        font-size: 18px;
+        margin: 0;
+    }
+</style>
