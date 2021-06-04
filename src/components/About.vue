ba0ac5ba143576c8f6567e3cf16dd4cc3db4f489
Kevin Jiang <kevin@popmeals.com>
2021-06-04 04:05:34 -0400

    Add animations

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,7 @@
   <div class="about">
       <div class="blurb">
         <p style="font-weight: 600; font-size: 24px;">Hello, I'm</p>
-        <p class="name" style="font-weight: bold; font-size: 64px;">Kevin Jiang</p>
+        <span class="name typing">Kevin Jiang</span>
         <p style="font-weight: bold; font-size: 24px;">MIT Computer Science Student</p>
         <div class="position">
           <img class="arrow" src="../assets/arrow.png"/>
@@ -25,11 +25,64 @@ export default {
 </script>
 
 <style scoped>
+@keyframes type {
+    0% {
+      width: 0;
+    }
+    1% {
+      opacity: 1;
+    }
+    99.9% {
+      border-right: 0.05em solid #FFFFFF;
+    }
+    100% {
+      opacity: 1;
+    }
+}
+
+@-webkit-keyframes type {
+    0% {
+      width: 0;
+    }
+    1% {
+      opacity: 1;
+    }
+    99.9% {
+      border-right: 0.05em solid #FFFFFF;
+    }
+    100% {
+      opacity: 1;
+    }
+}
+
+@keyframes blink {
+    from, to { border-color: transparent }
+    50% { border-color: #FFFFFF; }
+}
+
+@-webkit-keyframes blink {
+    from, to { border-color: transparent }
+    50% { border-color: #FFFFFF; }
+}
+
+@keyframes fadeInAnimation { 
+    0% { 
+        opacity: 0; 
+    } 
+    100% { 
+        opacity: 1; 
+     } 
+} 
+
 .about {
   background-color: var(--primary-main);
   display: flex;
   align-items: center;
   justify-content: center;
+
+    animation: fadeInAnimation ease 1.5s;
+    animation-iteration-count: 1; 
+    animation-fill-mode: forwards;
 }
 
 .navbar {
@@ -51,7 +104,29 @@ export default {
 }
 
 .name {
-    line-height: 1.5;
+    display: flex;
+    color: white;
+    white-space: nowrap;
+    overflow: hidden;
+    display: inline-block;
+    border-right: 0.05em solid #FFFFFF;
+
+    font-weight: bold; 
+    font-size: 64px;
+    margin: 1rem 0;
+
+    line-height: 1.3;
+}
+
+.typing {
+    width: 310px;
+    opacity: 0;
+    -webkit-animation: type 1s steps(20, end), blink 1s step-end infinite;
+    animation: type 1s steps(20, end), blink 1s step-end infinite;
+    -webkit-animation-delay: 0.5s;
+    animation-delay: 0.5s;
+    -webkit-animation-fill-mode: forwards;
+    animation-fill-mode: forwards;
 }
 
 .position {
