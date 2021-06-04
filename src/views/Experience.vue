60ea1e40a6f65c723d3d2dc8dd93fc461ee2fb2c
Kevin Jiang <kevin@popmeals.com>
2021-06-04 18:03:51 -0400

    Update Experience.vue

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,22 +1,41 @@
 <template>
   <div class="main">
     <Navbar class="navbar"/>
+    <Education class="card"/>
+    <Coursework class="card"/>
+    <Work class="card"/>
+    <p>hi :)</p>
   </div>
 </template>
 
 <script>
 import Navbar from "../components/Navbar.vue";
+import Education from "../components/Education.vue";
+import Coursework from "../components/Coursework.vue";
+import Work from "../components/Work.vue";
 
 export default {
   name: "Experience",
 
   components: {
-    Navbar
+    Navbar,
+    Education,
+    Coursework,
+    Work
   }
 };
 </script>
 
 <style scoped>
+@keyframes fadeInAnimation { 
+    0% { 
+        opacity: 0; 
+    } 
+    100% { 
+        opacity: 1; 
+     } 
+} 
+
 .main {
   background-color: var(--primary-main);
   display: flex;
@@ -28,5 +47,13 @@ export default {
 
 .navbar {
   width: 40%;
+}
+
+.card {
+    width: 70%;
+    margin: 1rem 0;
+    animation: fadeInAnimation ease 1s;
+    animation-iteration-count: 1; 
+    animation-fill-mode: forwards;
 }
 </style>
