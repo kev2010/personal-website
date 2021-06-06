5d1bf9b0a14882940c78d4bca5400b5702e030ae
Kevin Jiang <kevin@popmeals.com>
2021-06-05 22:56:25 -0400

    Update Projects.vue

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -11,10 +11,11 @@
                 <source :src="project.src"
                         type="video/mp4">
             </video>
-            <img v-else :src="project.src"/>
+            <img v-else width="100%" :src="project.src"/>
         </div>
         <div class="right-section">
-            <p style="font-weight: 300; font-size: 18px; margin-top: 2rem;" v-for="(paragraph, indexTwo) in project.description" :key="indexTwo">{{ paragraph }}</p>
+            <p v-for="(paragraph, indexTwo) in project.description" :key="indexTwo">{{ paragraph }}</p>
+            <a v-for="(link, indexThree) in project.links" :key="indexThree" :href="link.url" target="_blank">{{ link.text }}</a>
             <span style="margin-top: 2rem"/>
         </div>
       </div>
@@ -60,9 +61,35 @@ export default {
                       "Cofounder of first publicly accessible collegiate poker tournament. We created the Collegiate Series of Poker (CSOP) to bring together poker communities\
                       across the nation’s colleges. Our organization received $20,000 in funding from corporate sponsors. After marketing to US college students, we gained 457 signups across\
                       more than 20 different colleges. At the conclusion of our inaugural tournament on May 8th, 2021, we distributed $15,000 in prizes and shipped CSOP gear to participants\
-                      across the nation.",
-                      "Website: https://www.pokercsop.com/",
-                      "Facebook: https://www.facebook.com/PokerCSOP"
+                      across the nation."
+                    ],
+                    links: [
+                      {
+                        url: "https://www.pokercsop.com/",
+                        text: "Visit our website!"
+                      },
+                      {
+                        url: "https://www.facebook.com/PokerCSOP",
+                        text: "Check out our Facebook page"
+                      }
+                    ]
+                },
+                {
+                    title: "Angèl",
+                    date: "November 2020 - December 2020",
+                    video: false,
+                    src: require("../assets/angel.png"),
+                    description: [
+                      "Co-creator of Angèl, which provides a credit marketplace for underbanked Colombians using innovative and community-based metrics to assess creditworthiness. This\
+                       project was started to combat the effects of COVID-19 in third world countries such as Colombia. We executed a disciplined entrepreneurship framework by conducting\
+                       extensive primary market research with local Colombian business owners, assessing competitive advantage over existing solutions, and working through other business\
+                       considerations such as the TAM, full life cycle, MVP, customer LTV calculations, COCA, etc."
+                    ],
+                    links: [
+                      {
+                        url: "https://drive.google.com/file/d/1_yO3kydztqjLLkc0jwi_VS4j_Y-ELbYm/view?usp=sharing",
+                        text: "View our final presentation deck here"
+                      }
                     ]
                 },
                 {
@@ -74,9 +101,35 @@ export default {
                       "Co-creator of PoomsAI, an accuracy auto-grader for Taekwondo Poomsae using computer vision. Automating the accuracy grading helps make judging easier since\
                        human judges are easily error-prone due to the number of criteria to watch. Our approach consists of taking video footage of a competitor’s performance, \
                        determining the competitor’s pose through keypoint identification using OpenPose, and then analyzing the coordinates of the 2D pose estimation to provide \
-                       accuracy scores. Experiments show that our model performs on par with human judging on mediocre-to-good performances.",
-                      "GitHub: https://github.com/p1ck-4-u53rn4m3/poomsae-grader",
-                      "Paper: https://github.com/p1ck-4-u53rn4m3/poomsae-grader/blob/master/PoomsAI%20Paper.pdf"
+                       accuracy scores. Experiments show that our model performs on par with human judging on mediocre-to-good performances."
+                    ],
+                    links: [
+                      {
+                        url: "https://github.com/p1ck-4-u53rn4m3/poomsae-grader",
+                        text: "Our GitHub repository"
+                      },
+                      {
+                        url: "https://github.com/p1ck-4-u53rn4m3/poomsae-grader/blob/master/PoomsAI%20Paper.pdf",
+                        text: "Read our paper here :)"
+                      }
+                    ]
+                },
+                {
+                    title: "nPoint AI",
+                    date: "December 2020",
+                    video: false,
+                    src: require("../assets/npointAI.png"),
+                    description: [
+                      "Creator of nPointAI website using Vue. Co-developed the idea of nPointAI in order to provide hyper-personalization while still maintaining\
+                      privacy of user data. The goal was to utilize privacy preserving machine learning (PPML) and homomorphic encryption to provide businesses with recommendations\
+                      without revealing user data. A product using these technologies was not created in the end. Conducted primary market research with The Boston Globe and MIT.\
+                      Developed a prototype tracking pixel."
+                    ],
+                    links: [
+                      {
+                        url: "https://npoint.ai/",
+                        text: "Check out the website!"
+                      }
                     ]
                 },
                 {
@@ -87,8 +140,13 @@ export default {
                     description: [
                       "Co-creator of Safeskiii, which is a commenting and broadcasting app for Alta, Utah. A ski lounge can use it to inform its residents about current events that\
                        also incorporates a “check in” system for visitors to report their visiting time, which can help prevent crowding in this time of COVID. Done as a project\
-                       using VueJS, which is what this personal website was built on!",
-                      "Website: https://safeskiii-app.herokuapp.com/"
+                       using VueJS, which is what this personal website was built on!"
+                    ],
+                    links: [
+                      {
+                        url: "https://safeskiii-app.herokuapp.com/",
+                        text: "View our website"
+                      }
                     ]
                 },
                 {
@@ -98,8 +156,13 @@ export default {
                     src: require("../assets/mazescape.png"),
                     description: [
                       "Co-creator of Mazescape, a Pygame based game designed to introduce key concepts of robotics like localization, SLAM, and odometry to younger audiences\
-                       (e.g. high school freshman). The player is placed into a haunted maze and must find the pentagram to escape!",
-                      "GitHub: https://github.com/The-Ripp3r/Mazescape"
+                       (e.g. high school freshman). The player is placed into a haunted maze and must find the pentagram to escape!"
+                    ],
+                    links: [
+                      {
+                        url: "https://github.com/The-Ripp3r/Mazescape",
+                        text: "Check out our GitHub repository"
+                      }
                     ]
                 },
             ]
@@ -179,5 +242,12 @@ export default {
     justify-content: center;
     margin: 2rem 0 2rem 4rem;
     width: 40%;
+    font-weight: 300;
+    font-size: 18px;
+}
+
+.right-section p, .right-section a {
+  margin-top: 2rem;
+  color: white;
 }
 </style>
