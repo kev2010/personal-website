13710f759b38514bc9f7b9d07f40e46ab88ceb4a
Kevin Jiang <kevin@popmeals.com>
2021-06-06 02:28:11 -0400

    Add activities section

---
+++ src/components/ActivitiesNavbar.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="nav-bar">
+        <ul class="nav-links">
+            <li>
+                <router-link to="/activities/mit-clubs" class="section">MIT Clubs</router-link>
+            </li>
+            <li>
+                <router-link to="/activities/taekwondo" class="section">Taekwondo</router-link>
+            </li>
+            <li>
+                <router-link to="/activities/poker" class="section">Poker</router-link>
+            </li>
+            <li>
+                <router-link to="/activities/clubhouse" class="section">Clubhouse</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "ActivitiesNavbar"
+};
+</script>
+
+<style scoped>
+.nav-bar {
+    font-family: "Cairo";
+    font-style: normal;
+    font-weight: bold;
+    font-size: 18px;
+    margin-top: 1rem;
+}
+
+.nav-links {
+    list-style: none;
+    display: flex;
+    text-align: center;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0;
+}
+
+.nav-links li {
+    display: flex;
+    align-items: center;
+}
+
+.section {
+    text-decoration: none;
+    color: white;
+}
+
+.section:hover {
+    color: #BFBFBF;
+    text-decoration: var(--emphasis) solid underline;
+    text-decoration-thickness: 2px;
+    text-underline-position: under;
+}
+</style>
--- src/views/Activities.vue
+++ src/views/Activities.vue
@@ -1,32 +1,50 @@
 <template>
   <div class="main">
     <Navbar class="navbar"/>
+    <ActivitiesNavbar class="activities-navbar"/>
   </div>
 </template>
 
 <script>
 import Navbar from "../components/Navbar.vue";
+import ActivitiesNavbar from "../components/ActivitiesNavbar.vue";
 
 export default {
   name: "Activities",
 
   components: {
-    Navbar
+    Navbar,
+    ActivitiesNavbar
   }
 };
 </script>
 
 <style scoped>
+@keyframes fadeInAnimation { 
+    0% { 
+        opacity: 0; 
+    } 
+    100% { 
+        opacity: 1; 
+     } 
+} 
+
 .main {
   background-color: var(--primary-main);
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-between;
   height: 100%;
 }
 
 .navbar {
   width: 40%;
+}
+
+.activities-navbar {
+  width: 25%;
+  animation: fadeInAnimation ease 1s;
+  animation-iteration-count: 1; 
+  animation-fill-mode: forwards;
 }
 </style>
